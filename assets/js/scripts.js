8ba6c3705ed87c9be4c9808f51f1b9c10b96ad3d
Diego <da.infanta@gmail.com>
2021-09-30 18:29:20 -0300

    primera version

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+//asignamos un toggle para poder aparecer y desaparecer los textos de las recetas relacionadas al hace click en su titulo.
+
+$("#card1").click(function() {
+    $("#p1").toggle();
+  });
+  
+  $("#card2").click(function() {
+    $("#p2").toggle();
+  });
+  
+  $("#card3").click(function() {
+    $("#p3").toggle();
+  });
+  $("#card4").click(function() {
+    $("#p4").toggle();
+  });
